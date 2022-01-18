c2481178b12c5f9028f124b361ce85c7aa9f6233
Francesco Lazzarotto <fralaz1971@gmail.com>
2022-01-18 12:28:55 +0100

    added installation Makefile specific for GNU/Linux

--- setup.sh
+++ setup.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env sh
+PKGNAME="hellobye"
 autoreconf --install
-./configure --prefix=$HOME/Software/hellobye
-make
-make install
-echo 'export PATH=$PATH:$HOME/Software/hellobye/bin' >> $HOME/.bashrc
-if test -e $HOME/.bash_profile
-then
-	echo 'export PATH=$PATH:$HOME/Software/hellobye/bin' >> $HOME/.bash_profile
-fi
+cp Makefile_lnx.am Makefile.am
+make -i -f Makefile.lnx distclean
+./configure --prefix="$HOME"/Software/"$PKGNAME"
+find . -type f -name Makefile -exec sed -i 's/MAKE=mingw32-make/MAKE=make/g' {} \;
+find . -type f -name Makefile -exec  cp {} {}.lnx \;
+find . -type f -name Makefile.lnx -exec sed -i 's/MAKE=mingw32-make/MAKE=make/g' {} \;
+make -f Makefile.lnx -i
+make -f Makefile.lnx -i install
+echo 'export PATH=$PATH:$HOME/Software/hellobye/bin' >> "$HOME"/.bashrc
+echo 'export PATH=$PATH:$HOME/Software/hellobye/bin' >> "$HOME"/.bash_profile
+make -f Makefile.lnx -i distclean
+echo "***********************************************"
+echo "*** the $PKGNAME package has been installed"
+echo "*** in $HOME/Software/hellobye"
+echo "***********************************************"
