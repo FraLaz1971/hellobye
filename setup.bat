c48faf6896cb1cb4cc19eaebc770b514b84d0d8e
Francesco Lazzarotto <fralaz1971@gmail.com>
2022-01-18 10:38:52 +0100

    improved setup.bat

--- setup.bat
+++ setup.bat
@@ -1,8 +1,12 @@
-#!/usr/bin/env sh
-autoreconf --install
+REM this script installs the hellobye project
+@echo off
+REM autoreconf --install
 configure.bat /prefix=$HOME/Software/hellobye
 mingw32-make
 mingw32-make install
+REM ***********************
+REM *** TO BE COMPLETED ***
+REM ***********************
 REM echo 'export PATH=$PATH:$HOME/Software/hellobye/bin' >> $HOME/.bashrc
 REM if test -e $HOME/.bash_profile
 REM then
